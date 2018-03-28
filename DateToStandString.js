95778b2c06d47480e70be9d0753faa4d52435eee
aoarashi1988 <30466105+aoarashi1988@users.noreply.github.com>
2018-03-28 16:48:05 +0800

    将时间格式处理为ISO标准，当前时区的格式

---
+++ DateToStandString.js
@@ -0,0 +1,20 @@
+//输出和ISO标准一样格式的时间，然而时区调整为当前时区
+
+(function(){
+  function pad(number) {
+      if (number < 10) {
+          return '0' + number;
+      }
+      return number;
+  }
+  Date.prototype.toStandString = function () {
+      return this.getFullYear() +
+          '-' + pad(this.getMonth() + 1) +
+          '-' + pad(this.getDate()) +
+          'T' + pad(this.getHours()) +
+          ':' + pad(this.getMinutes()) +
+          ':' + pad(this.getSeconds()) +
+          '.' + (this.getMilliseconds() / 1000).toFixed(3).slice(2, 5) +
+          'Z';
+  };
+})()
